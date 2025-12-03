b2dd8bf1bd5bde7a48fb60dcb1b0e3ade4dc1c3d
nguyenluat19 <128703641+nguyenluat19@users.noreply.github.com>
2025-12-03 07:54:31 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ProductManager from "./components/ProductManager";
 export default function App() {
   return (
     <div className="container">
-      <h1 style={{ textAlign: "center" }}>Quản Lý Sản Phẩm</h1>
+      <h1 style={{ textAlign: "center" }}>Quản Lý Sản Phẩm Của Trần Nữ Hồ Na</h1>
       <ProductManager />
     </div>
   );
